7ee32fc57867a92013407253700bab8904031440
Pywww <Pywww@github.com>
2019-04-24 10:01:32 +0800

    update5/确定用户管理页面

--- mysite/apps/user/models.py
+++ mysite/apps/user/models.py
@@ -7,7 +7,10 @@ class User(AbstractUser):
     """
     用户模型
     """
+    id = models.IntegerField(primary_key=True,verbose_name='用户编号')
     telephone = models.CharField(max_length=11, verbose_name='手机号',default='')
+    password = models.CharField(max_length=10,verbose_name='密码',default='')
+    icon = models.ImageField(upload_to='img',default='')
 
     class Meta:
         verbose_name = '用户模型'
--- mysite/apps/user/views.py
+++ mysite/apps/user/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+from .models import User
+
 
 class Index_view(View):
-    def get(self,request):
-        return render(request,'user/index.html')
+    def get(self, request):
+        return render(request, 'user/index.html')
+
 
 class UserView(View):
     def get(self, request):
@@ -22,5 +25,19 @@ class User_manage(View):
     支持用户更换头像
     用户修改密码
     '''
+
     def get(self, request):
-        return render(request,'user/user_manage.html')
+        return render(request, 'user/user_manage.html')
+
+    def post(self, request):
+        '''
+        用户修改个人信息
+        修改手机号码和用户名
+        '''
+        name = request.POST.get('name','')
+        telephone = request.POST.get('telephone','')
+        user = User.objects.filter(id=id)
+        user.telephone = telephone
+        user.name=name
+        user.save()
+        return HttpResponse('ok')
